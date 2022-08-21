c5f806dbd938c4f4aced3ce85f7c5c38baf35dbb
chithiraivel <chithiraivels11@gmail.com>
2022-08-21 22:44:19 +0530

    javascript-TO-DO-LIST.

---
+++ To-do-list.js
@@ -0,0 +1,28 @@
+
+let myForm = document.getElementById("myForm")
+let myInput = document.getElementById("myInput")
+let myList = document.getElementById("myList") 
+
+myForm.addEventListener("submit", (data) => {
+
+            data.preventDefault()
+            createItem(myInput.value) })
+            
+  createItem = (d) => {
+
+    let myTemplate=`<li> ${d} <button onclick="deleteItem(this)">Delete</button></li>`
+    myList.insertAdjacentHTML("beforeend",myTemplate)
+
+    myInput.value="";
+    
+    myInput.focus();
+  }
+
+  function deleteItem(x){
+
+    x.parentElement.remove()
+
+  }
+
+
+
